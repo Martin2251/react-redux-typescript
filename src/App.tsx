8dfc03ec98d847cb4dde070d12af58aee8cf3ae8
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-17 11:33:45 +0100

    notes reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { NewNoteInput } from './components/NewNoteinput';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { NewNoteInput } from "./components/NewNoteinput";
+import { NotesState } from "./notesReducer";
 
 function App() {
+  const notes = useSelector<NotesState, NotesState["notes"]>(
+    (state) => state.notes
+  );
   return (
     <>
-<NewNoteInput addNote={alert}  />
- <hr  />
- <ul>
-   <li>some notes</li>
- </ul>
- </>
+      <NewNoteInput addNote={alert} />
+      <hr />
+      <ul>
+        {notes.map((note) => {
+          return <li key={note}>some {note}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
